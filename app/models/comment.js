ca7439c4a6e6931f4584a885543aad1fbd7eda7c
Joey Arnstein and lindsey raffa <lindsey.raffa@gmail.com>
2016-04-06 16:48:43 -0700

    add nothing

--- app/models/comment.js
+++ app/models/comment.js
@@ -3,5 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr(),
   content: DS.attr(),
-  product: DS.belongsTo('product', {async: true }),
+  products: DS.belongsTo('product', {async: true }),
 });
